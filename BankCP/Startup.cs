9bdd8c1bb088c86501f88ad1bf373cc3af34e346
MoayadRaddad <76817188+MoayadRaddad@users.noreply.github.com>
2021-03-01 09:30:13 +0200

    .

--- BankCP/Startup.cs
+++ BankCP/Startup.cs
@@ -22,7 +22,6 @@ namespace BankCP
     public class Startup
     {
         public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
-        public static string PublicClientId { get; private set; }
         public void Configuration(IAppBuilder app)
         {
             try
